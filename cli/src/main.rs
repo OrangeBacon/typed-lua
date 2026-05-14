9bcf41d6cb4dd8cc1a2def67aa7d2e0bfa982837
Ella <ella.moreton20@gmail.com>
2026-05-14 15:56:46 +0000

    Add simple cli to make testing easier

---
+++ cli/src/main.rs
@@ -0,0 +1,20 @@
+use std::error::Error;
+
+/// Incredibly basic commandline code runner
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = std::env::args();
+    args.next();
+
+    let path = match args.next() {
+        Some(f) => f,
+        None => "main.tlua".into(),
+    };
+
+    let src = std::fs::read_to_string(path)?;
+
+    let lexer = typed_lua::Lexer::new(&src);
+
+    println!("{lexer}");
+
+    Ok(())
+}
--- typed-lua/src/lib.rs
+++ typed-lua/src/lib.rs
@@ -1,14 +1,3 @@
 mod parser;
 
 pub use parser::lexer::Lexer;
-
-pub fn run() {
-    println!("{}", Lexer::new(";^:a and b&--[==[aaa]===]]==]::--hi\n--[[]]\"a\\\"\";"));
-}
-
-#[cfg(test)]
-#[test]
-fn it_works() {
-    run();
-    todo!();
-}
